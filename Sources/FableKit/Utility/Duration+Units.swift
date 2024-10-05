e8e111b672f3645080cd111f893075fbc8234530
Jae Kong <hello@jtoda.one>
2024-10-05 22:19:04 +0900

    fix: make functions of duration extension public

--- Sources/FableKit/Utility/Duration+Units.swift
+++ Sources/FableKit/Utility/Duration+Units.swift
@@ -1,43 +1,43 @@
 import Foundation
 
 public extension Duration {
-    static func hours<T: BinaryInteger>(_ hours: T) -> Duration {
+    public static func hours<T: BinaryInteger>(_ hours: T) -> Duration {
         return .minutes(hours * T(60))
     }
 
-    static func hours(_ hours: Double) -> Duration {
+    public static func hours(_ hours: Double) -> Duration {
         return .minutes(hours * 60)
     }
 
-    static func minutes<T: BinaryInteger>(_ minutes: T) -> Duration {
+    public static func minutes<T: BinaryInteger>(_ minutes: T) -> Duration {
         return .seconds(minutes * T(60))
     }
 
-    static func minutes(_ minutes: Double) -> Duration {
+    public static func minutes(_ minutes: Double) -> Duration {
         return .seconds(minutes * 60)
     }
 
-    static func frames<T: BinaryInteger>(_ frames: T, fps: Int = 60) -> Duration {
+    public static func frames<T: BinaryInteger>(_ frames: T, fps: Int = 60) -> Duration {
         return .seconds(Double(frames) / Double(fps))
     }
     
-    static func frames(_ frames: Double, fps: Int = 60) -> Duration {
+    public static func frames(_ frames: Double, fps: Int = 60) -> Duration {
         return .seconds(frames / Double(fps))
     }
 
-    func hours(_ hours: Double) -> Duration {
+    public func hours(_ hours: Double) -> Duration {
         self + .hours(hours)
     }
 
-    func minutes(_ minutes: Double) -> Duration {
+    public func minutes(_ minutes: Double) -> Duration {
         self + .minutes(minutes)
     }
 
-    func seconds(_ seconds: Double) -> Duration {
+    public func seconds(_ seconds: Double) -> Duration {
         self + .seconds(seconds)
     }
 
-    func frames(_ frames: Double, fps: Int = 60) -> Duration {
+    public func frames(_ frames: Double, fps: Int = 60) -> Duration {
         self + .frames(frames, fps: fps)
     }
 }
